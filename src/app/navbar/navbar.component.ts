54cca2f2916b16860017868787be7114a73a3f42
chavdar.momchilov <chavmom@gmail.com>
2023-03-07 12:01:50 -0500

    Changes to be committed: 	modified:   src/app/navbar/navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-navbar',
-    template: '<h1>Navbar Component</h1>'
+    template: '<h1>Navbar Component</h1>',
+    styles:['h1{color: red}']
 })
 
 export class NavbarComponent{}
