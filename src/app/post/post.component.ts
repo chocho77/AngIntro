a0cec82aa93b1aa5981efc11be1c47ffd773359e
chavdar.momchilov <chavmom@gmail.com>
2023-03-08 06:27:49 -0500

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/post/post.component.html 	modified:   src/app/post/post.component.ts

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit{
+
+  title:string = "Lists of Posts";
+
+  @Input()
+  fromParent!: string;
+
+  constructor(){}
+
+
+  ngOnInit(): void {
+    
+  }
 
 }
